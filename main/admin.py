81125a5169f05c52804c75b1cc9186b3a30fc29c
Александр Григорьев <grishanewman@mail.ru>
2024-06-09 18:54:20 +0700

    first_base

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MainModels
 
-# Register your models here.
+@admin.register(MainModels)
+
+class MainModelsAdmin(admin.ModelAdmin):
+    list_display=["title","name","content"]
--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class MainModels(models.Model):
     title = models.CharField(max_length=20, verbose_name="Заголовок")
     name = models.CharField(max_length=100, verbose_name="Имя страницы")
-    content = models.TextField(verbose_name="Контент")
+    content = models.TextField(verbose_name="Контент",blank=True,null=True)
 
     class Meta:
         verbose_name = "Главная страница"
