bd423f36020f25d31534b441a20c04457c43f5ea
Alexandr Grigoryev <grishanewman@mail.ru>
2024-06-19 12:51:59 +0700

    Category_models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    fields=['name','slug']
+    prepopulated_fields={'slug':('name',)}
+
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name=models.CharField(max_length=30,verbose_name='Имя категории')
+    slug=models.SlugField(max_length=20,unique=True, verbose_name="Слаг")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+        def __str__(self):
+            return f"{self.name}"
 # Create your models here.
