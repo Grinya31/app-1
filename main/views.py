ecc3986b4abf512150934099ad64ad6274a66a74
Alexandr Grigoryev <grishanewman@mail.ru>
2024-06-19 17:57:30 +0700

    shablon category

--- main/views.py
+++ main/views.py
@@ -8,3 +8,9 @@ def index(request):
 
 def about(request):
     return HttpResponse('О нас')
+
+def contact(request):
+    return HttpResponse("Контакт")
+
+def dostavka(request):
+    return HttpResponse('Доставка')
