cc49330f1900e9a3cc3ba57ca83da6b66158e408
Alexandr Grigoryev <grishanewman@mail.ru>
2024-06-20 15:56:20 +0700

    models goods category

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categories,Goodsmodels
 
-# Register your models here.
+@admin.register(Categories)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    fields=['name','slug']
+    prepopulated_fields={'slug':('name',)}
+
+
+@admin.register(Goodsmodels)
+class GoodsAdmin(admin.ModelAdmin):
+    list_display=['name','slug',]
+    fields=['name','slug']
+    prepopulated_fields={'slug':('name',)}
+
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Categories(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    slug=models.SlugField(max_length=200,unique=True,blank=True,null=True)
+
+    class Meta:
+        db_table="Category"
+        verbose_name="Категория"
+        verbose_name_plural="Категории"
+
+
+
+class Goodsmodels(models.Model):
+    name=models.CharField(max_length=200,verbose_name='Название товара')
+    slug=models.SlugField(verbose_name='слаг',unique=True)
+    description=models.TextField(verbose_name='Описание товара',blank=True,null=True)
+    price=models.DecimalField(default=0.00,max_digits=7,decimal_places=2,verbose_name='Стоимость')
+    image=models.ImageField(upload_to='goods_images')
+    discount=models.DecimalField(default=0.00,max_digits=7,decimal_places=2,verbose_name='Скидка')
+    quantity=models.PositiveIntegerField(default=0,verbose_name='Количество')
+    category=models.ForeignKey(Categories,on_delete=models.PROTECT)
+    
+    
+    class Meta:
+        db_table="Goods"
+        verbose_name="Продукт"
+        verbose_name_plural="Продукты"
